44921ed64a9c26783dea5e40f0bf6fd23698e463
SammyLeths <s.o.eyiowuawi@gmail.com>
2023-04-24 16:53:41 +0100

    added products app

---
+++ backend/api/views.py
@@ -0,0 +1,16 @@
+import json
+from django.forms.models import model_to_dict
+from django.http import JsonResponse, HttpResponse
+from products.models import Product
+
+def api_home(request, *args, **kwargs):
+  model_data = Product.objects.all().order_by("?").first()
+  data = {}
+  if model_data:
+    data = model_to_dict(model_data, fields=['id', 'title', 'price'])
+  return JsonResponse(data)
+  #   print(data)
+  #   data = dict(data)
+  #   json_data_str = json.dumps(data)
+    
+  # return HttpResponse(json_data_str, headers={'content-type': 'application/json'})
--- py_client/basic.py
+++ py_client/basic.py
@@ -1,12 +1,17 @@
 import requests
 
-endpoint = "https://httpbin.org/status/200/"
-endpoint = "https://httpbin.org/anything"
+# endpoint = "https://httpbin.org/status/200/"
+# endpoint = "https://httpbin.org/anything"
+endpoint = "http://localhost:8000/api/"
 
-get_response = requests.get(endpoint) # HTTP Request
-print(get_response.text) # print raw text response
+get_response = requests.get(endpoint, params={"abc": 123}, json={'query': 'Hello world'}) # HTTP Request
+# print(get_response.headers)
+# print(get_response.text) # print raw text response
+# print(get_response.status_code)
 
 # HTTP Request -> HTML
 # REST API HTTP Request -> JSON
 
 # JavaScript Object Notation ~ Python Dict
+
+print(get_response.json())
