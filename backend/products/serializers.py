7b040e98cddb4941bb01e7c199759e2277f3878a
SammyLeths <s.o.eyiowuawi@gmail.com>
2023-04-27 14:11:10 +0100

    updated serializers

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -2,14 +2,28 @@ from rest_framework import serializers
 from rest_framework.reverse import reverse
 from .models import Product
 from .validators import validate_title, validate_title_no_hello, unique_product_title
+from api.serializers import UserPublicSerializer
+
+
+class ProductInlineSerializer(serializers.Serializer):
+    url = serializers.HyperlinkedIdentityField(
+        view_name='product-detail',
+        lookup_field='pk',
+        read_only=True
+    )
+    title = serializers.CharField(read_only=True)
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    owner = UserPublicSerializer(source="user", read_only=True)
+    related_products = ProductInlineSerializer(
+        source="user.product_set.all", read_only=True, many=True)
+    my_user_data = serializers.SerializerMethodField(read_only=True)
     my_discount = serializers.SerializerMethodField(read_only=True)
     edit_url = serializers.SerializerMethodField(read_only=True)
     url = serializers.HyperlinkedIdentityField(
         view_name='product-detail', lookup_field='pk')
-    # email = serializers.EmailField(write_only=True)
+    email = serializers.EmailField(source="user.email", write_only=True)
     title = serializers.CharField(
         validators=[validate_title_no_hello, unique_product_title])
     name = serializers.CharField(source='title', read_only=True)
@@ -17,7 +31,8 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = [
-            # 'user',
+            'owner',
+            'email',
             'url',
             'edit_url',
             'pk',
@@ -27,7 +42,14 @@ class ProductSerializer(serializers.ModelSerializer):
             'price',
             'sale_price',
             'my_discount',
+            'my_user_data',
+            'related_products',
         ]
+
+    def get_my_user_data(self, obj):
+        return {
+            "username": obj.user.username
+        }
 
     # def validate_title(self, value):
     #     request = self.context.get('request')
--- py_client/create.py
+++ py_client/create.py
@@ -4,8 +4,8 @@ headers = {'Authorization': 'Bearer 3eb47671be1e94bd2427bc3cec6487c4b38dd6c5'}
 endpoint = "http://localhost:8000/api/products/"
 
 data = {
-  'title': 'This field is done',
-  'price': 32.99
+    'title': 'This field is done',
+    'price': 32.99
 }
 get_response = requests.post(endpoint, json=data, headers=headers)
-print(get_response.json())
+# print(get_response.json())
